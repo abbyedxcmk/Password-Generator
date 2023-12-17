af5a5756010d8dccfae391c454a6aa7a203def07
Isiaka Bhadmus <swiftnetuk@icloud.com>
2023-12-17 16:19:27 +0000

    Added: Password length input and validation

--- script.js
+++ script.js
@@ -33,40 +33,51 @@ const generatedPassword = '';
 // as an array and push each character, then join once you have enough characters
 
 // Function to prompt user for password options
+
 function getPasswordOptions() {
-    // Prompt for password length
-    // At least 8 characters, no more than 128 characters
-    // Conditional to check that the number that was entered is in range
-    // Prompts store data as strings, so need to parse into a number
-    // If the user's input is out of range, either return out of the function or call the function again
-  
+    let passwordLength;
+    while (true) {
+        // Prompt for password length
+        passwordLength = prompt('Enter the desired password length (8-128)');
+        passwordLength = parseInt(passwordLength, 10);
+        // At least 8 characters, no more than 128 characters
+        // Conditional to check that the number that was entered is in range
+        // Prompts store data as strings, so need to parse into a number
+        // If the user's input is out of range, either return out of the function or call the function again
+        if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+            alert("Invalid input. Length must be between 8 and 128.");
+        } else {
+            break;
+        }
+    }
+   
     // Confirm which character sets to use
     // If the user answers false for all, either return out of the function or call the function again
-    
+
     // Once they select a character set:
     // Generate a random character for each selected character set
     // Either push selected character sets to a mega-array of all selected characters
     // OR you can keep the arrays separate and generate a random number to select the array and another to select the index
-    
+
     // Once character sets are selected, move on to generating random characters
-  }
-  
-  // Function for getting a random element from an array
-  function getRandom(arr) {
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
     // Need a variable to hold the password as it's being generated
     // Need a variable to hold the index that's being generated
-  
+
     // For loop that loops the number of times that matches the length the user chose
     // Generate a random number
     // That number is the index for a character in the mega-array
     // So then, mega-array[generated-index] is the actual character
     // Add that character to the password
-  
-    // Once we finish the for loop, return the generated password
-  
-   }
 
-   // Get references to the #generate element
+    // Once we finish the for loop, return the generated password
+
+}
+getPasswordOptions();
+// Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
